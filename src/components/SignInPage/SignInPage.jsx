c5b09dc2c4c01fd0d95dae952e4f7fc8dd66ab7b
sassnemu1 <72015585+sassnemu1@users.noreply.github.com>
2021-03-02 23:27:22 +0300

    Add files via upload

---
+++ src/components/SignInPage/SignInPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SignInPage.css'
+
+
+const SignInPage = () => {
+    return (
+        <>
+            <div className="sign-in__page">
+        
+            </div>
+        </>
+    )
+}
+
+export default SignInPage
